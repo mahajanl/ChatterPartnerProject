6c7450f012b339393033678c6992f2e35d836dad
mahajanl <lakshamima@gmail.com>
2017-11-13 13:27:31 -0500

    initial commit

---
+++ ChatterMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatterMVC.Startup))]
+namespace ChatterMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
